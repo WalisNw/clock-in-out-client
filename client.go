5c4b490b30dab0dbd292965b0a519517806ed72e
walis <walis@nw.idv.tw>
2021-07-03 12:17:25 +0800

    feat: 查詢功能 1. 新增查詢功能 (日/週/月) 2. 調整文件

--- client.go
+++ client.go
@@ -47,6 +47,10 @@ const (
 	RegularTermination = "end"
 	AlertInterval      = 480
 	CountDownInterval  = 300
+	DateLayout = "2006/01/02(Mon)"
+	TimeLayout = "15:04:05"
+	DateTimeLayout = "2006/01/02 15:04:05"
+	NoRecord = "未打卡"
 )
 
 var (
@@ -63,14 +67,25 @@ var (
 	uncheckedImage *ebiten.Image
 )
 
-type Flag uint8
+type Flag uint16
 
 const (
 	Connecting Flag = 1 << iota
 	Loading
 	Select
-	Punching
+	Clock
+	Record
+	Clocking
+	Querying
 	CountDown
+	Result
+)
+
+type Func uint8
+
+const (
+	FuncClock Func = 1 << iota
+	FuncQuery
 )
 
 func init() {
@@ -117,10 +132,13 @@ type Game struct {
 	gRPC      *gRPC
 	counter   uint16
 	flag      Flag
+	funcFlag  Func
 	tick      uint
 	alert     string
 	msg       string
-	clockType pb.Type
+	clockType pb.ClockType
+	queryType pb.QueryType
+	records []record
 }
 
 func (g *Game) Update() error {
@@ -155,13 +173,28 @@ func (g *Game) Update() error {
 			g.gRPC.conn = conn
 			g.gRPC.client = pb.NewClockServiceClient(conn)
 			g.flag = Select
+			g.funcFlag = FuncClock
 		}()
 	case Loading:
 		g.msg = "連線中"
 	case Select:
 		switch {
 		case repeatingKeyPressed(ebiten.KeyEnter), repeatingKeyPressed(ebiten.KeyArrowRight):
-			g.flag = Punching
+			switch g.funcFlag {
+			case FuncClock:
+				g.flag = Clock
+			case FuncQuery:
+				g.flag = Record
+			}
+		case repeatingKeyPressed(ebiten.KeyArrowUp):
+			g.funcFlag = FuncClock
+		case repeatingKeyPressed(ebiten.KeyArrowDown):
+			g.funcFlag = FuncQuery
+		}
+	case Clock:
+		switch {
+		case repeatingKeyPressed(ebiten.KeyEnter), repeatingKeyPressed(ebiten.KeyArrowRight):
+			g.flag = Clocking
 			go func() {
 				ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 				defer cancel()
@@ -174,21 +207,69 @@ func (g *Game) Update() error {
 					g.flag = Select
 					return
 				}
-				g.msg = fmt.Sprintf("%v %v", res.Result, res.Time.AsTime().Local().Format("2006/01/02 15:04:05"))
+				g.msg = fmt.Sprintf("%v %v", res.Result, res.Time.AsTime().Local().Format(DateTimeLayout))
 				g.flag = CountDown
 				g.tick = CountDownInterval
 			}()
 		case repeatingKeyPressed(ebiten.KeyArrowUp):
-			g.clockType = pb.Type_CLOCK_IN
+			g.clockType = pb.ClockType_CLOCK_IN
 		case repeatingKeyPressed(ebiten.KeyArrowDown):
-			g.clockType = pb.Type_CLOCK_OUT
+			g.clockType = pb.ClockType_CLOCK_OUT
+		case repeatingKeyPressed(ebiten.KeyArrowLeft):
+			g.flag = Select
 		}
-	case Punching:
+	case Record:
+		switch {
+		case repeatingKeyPressed(ebiten.KeyEnter), repeatingKeyPressed(ebiten.KeyArrowRight):
+			g.flag = Querying
+			go func() {
+				ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+				defer cancel()
+				id, _ := strconv.Atoi(_id)
+				res, err := g.gRPC.client.Query(ctx, &pb.QueryRequest{Member: &pb.Member{Id: int32(id)}, Type: g.queryType})
+				if err != nil {
+					log.Printf("Failed to query records. err: %v", err)
+					g.alert = "查詢失敗"
+					g.tick = AlertInterval
+					g.flag = Select
+					return
+				}
+				records := make([]record, 0, len(res.Records))
+				for _, r := range res.Records {
+					record := record{in: NoRecord, out: NoRecord}
+					record.date = r.Date.AsTime().Local()
+					if r.In.IsValid() {
+						record.in = r.In.AsTime().Local().Format(TimeLayout)
+					}
+					if r.Out.IsValid() {
+						record.out = r.Out.AsTime().Local().Format(TimeLayout)
+					}
+					records = append(records, record)
+				}
+				g.records = records
+				g.flag = Result
+			}()
+		case repeatingKeyPressed(ebiten.KeyArrowUp):
+			if g.queryType != pb.QueryType_DAY {
+				g.queryType--
+			}
+		case repeatingKeyPressed(ebiten.KeyArrowDown):
+			if g.queryType != pb.QueryType_MONTH {
+				g.queryType++
+			}
+		case repeatingKeyPressed(ebiten.KeyArrowLeft):
+			g.flag = Select
+		}
+	case Clocking, Querying:
 		g.msg = "請稍候"
 	case CountDown:
 		if repeatingKeyPressed(ebiten.KeyEnter) || g.tick == 0 {
 			fmt.Println("Shutdown!")
 			return errors.New(RegularTermination)
+		}
+	case Result:
+		if repeatingKeyPressed(ebiten.KeyArrowLeft) {
+			g.flag = Record
 		}
 	}
 	g.counter++
@@ -200,13 +281,29 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	text.Draw(screen, fmt.Sprintf("現在時間: %s", time.Now().Format("2006/01/02 15:04:05")), regularFont, Padding, Padding+Row, color.White)
+	text.Draw(screen, fmt.Sprintf("現在時間: %s", time.Now().Format(DateTimeLayout)), regularFont, Padding, Padding+Row, color.White)
 	text.Draw(screen, g.alert, regularFont, Padding, Padding+Row*2, color.RGBA64{R: 0xffff, A: 0xff00})
 	switch g.flag {
 	case Select:
 		text.Draw(screen, "請選擇:", regularFont, Padding, Padding+Row*5, color.White)
 		op := &ebiten.DrawImageOptions{}
-		if g.clockType == pb.Type_CLOCK_IN {
+		if g.funcFlag == FuncClock {
+			op.GeoM.Translate(Padding, Row*7+2)
+			screen.DrawImage(checkedImage, op)
+			op.GeoM.Translate(0, Row)
+			screen.DrawImage(uncheckedImage, op)
+		} else {
+			op.GeoM.Translate(Padding, Row*8+2)
+			screen.DrawImage(checkedImage, op)
+			op.GeoM.Translate(0, Row*-1)
+			screen.DrawImage(uncheckedImage, op)
+		}
+		text.Draw(screen, "打卡", regularFont, Padding+20, Padding+Row*7, color.White)
+		text.Draw(screen, "查詢", regularFont, Padding+20, Padding+Row*8, color.White)
+	case Clock:
+		text.Draw(screen, "請選擇:", regularFont, Padding, Padding+Row*5, color.White)
+		op := &ebiten.DrawImageOptions{}
+		if g.clockType == pb.ClockType_CLOCK_IN {
 			op.GeoM.Translate(Padding, Row*7+2)
 			screen.DrawImage(checkedImage, op)
 			op.GeoM.Translate(0, Row)
@@ -219,18 +316,61 @@ func (g *Game) Draw(screen *ebiten.Image) {
 		}
 		text.Draw(screen, "上班打卡", regularFont, Padding+20, Padding+Row*7, color.White)
 		text.Draw(screen, "下班打卡", regularFont, Padding+20, Padding+Row*8, color.White)
-	case Loading, Punching:
+	case Record:
+		text.Draw(screen, "請選擇:", regularFont, Padding, Padding+Row*5, color.White)
+		op := &ebiten.DrawImageOptions{}
+		if g.queryType == pb.QueryType_DAY {
+			op.GeoM.Translate(Padding, Row*7+2)
+			screen.DrawImage(checkedImage, op)
+			op.GeoM.Translate(0, Row)
+			screen.DrawImage(uncheckedImage, op)
+			op.GeoM.Translate(0, Row)
+			screen.DrawImage(uncheckedImage, op)
+		} else if g.queryType == pb.QueryType_WEEK{
+			op.GeoM.Translate(Padding, Row*7+2)
+			screen.DrawImage(uncheckedImage, op)
+			op.GeoM.Translate(0, Row)
+			screen.DrawImage(checkedImage, op)
+			op.GeoM.Translate(0, Row)
+			screen.DrawImage(uncheckedImage, op)
+		} else {
+			op.GeoM.Translate(Padding, Row*7+2)
+			screen.DrawImage(uncheckedImage, op)
+			op.GeoM.Translate(0, Row)
+			screen.DrawImage(uncheckedImage, op)
+			op.GeoM.Translate(0, Row)
+			screen.DrawImage(checkedImage, op)
+		}
+		text.Draw(screen, "本日", regularFont, Padding+20, Padding+Row*7, color.White)
+		text.Draw(screen, "本週", regularFont, Padding+20, Padding+Row*8, color.White)
+		text.Draw(screen, "本月", regularFont, Padding+20, Padding+Row*9, color.White)
+
+	case Loading, Clocking, Querying:
 		msg := g.msg
 		msg += strings.Repeat(".", int(g.counter)%180/30)
 		text.Draw(screen, msg, regularFont, Padding, Padding+Row*5, color.White)
 	case CountDown:
 		text.Draw(screen, g.msg, regularFont, Padding, Padding+Row*5, color.White)
 		text.Draw(screen, fmt.Sprintf("將於 %d 秒後自動關閉或按<Enter>直接關閉", (g.tick/60)+1), regularFont, Padding, Padding+Row*7, color.White)
+	case Result:
+		text.Draw(screen, "日期", regularFont, Padding, Padding+Row*3, color.White)
+		text.Draw(screen, "上班打卡", regularFont, 200, Padding+Row*3, color.White)
+		text.Draw(screen, "下班打卡", regularFont, 340, Padding+Row*3, color.White)
+		for i, r := range g.records {
+			text.Draw(screen, r.date.Format(DateLayout), regularFont, Padding, Padding+Row*(4+i), color.White)
+			text.Draw(screen, r.in, regularFont, 200, Padding+Row*(4+i), color.White)
+			text.Draw(screen, r.out, regularFont, 340, Padding+Row*(4+i), color.White)
+		}
 	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return outsideWidth, outsideHeight
+}
+
+type record struct {
+	date time.Time
+	in, out string
 }
 
 type gRPC struct {
